6b528153ec82521c1be67c33cee418a90897eb5d
annapolec <ania.polec@gmail.com>
2015-09-10 00:58:43 +0200

    Error messages, Car validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,13 @@
 class Car < ActiveRecord::Base
 	has_many :rentings
 
+	validates :name, :description, :category, :price, presence: true
+	validates :name, length: { maximum: 30 }
+	validates :description, length: { maximum: 300 }
+	validates :category, inclusion: { in: ["A", "B", "C"] }
+	validates :price, numericality: { only_integer: true, greater_than: 0 }
+
+
 	def assign_price
 		prices = {	"A" => 100, 
 								"B" => 75,
