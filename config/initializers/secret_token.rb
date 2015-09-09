3d460940a4a7dcc849120569f71b0228d977b231
annapolec <ania.polec@gmail.com>
2015-09-09 14:36:51 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RentACar::Application.config.secret_key_base = '7a7a0f9fa0b5efe06705b23a37933b2ac02e36242c4a03280ac6ddad0516c2fb965e60ecc88b02cce7746875e3c30cd946ffe8301fd3dd384b2bd75529fd3715'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RentACar::Application.config.session_store :cookie_store, key: '_rent_a_car_session'
