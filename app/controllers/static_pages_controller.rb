9ce7e21410821bb80dbd6eb83b0af7f0245f27cd
annapolec <ania.polec@gmail.com>
2015-09-13 17:59:25 +0200

    Add validation to search form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,12 @@
 class StaticPagesController < ApplicationController
+	before_filter :authenticate_user!
 	def home		
 	end
 
 	def search_results
-		if params[:length].empty?
-			params[:length] = 1
-		end		
 		@booking = Booking.new(start_time: params[:start_time], length: params[:length])
 		@booking.calculate_end_time
 		@cars = Car.all
-		render :partial => 'search_results', :content_type => 'text/html'
+		render :partial => 'search_results', :content_type => 'text/html'	
 	end
 end
